1fff44e63d0689ff248b27c593e19a10b7aad11c
Pepino <cardiacchino@gmail.com>
2020-06-28 22:54:39 -0500

    al menos corre

--- Skip_Backend/src/main/java/com/simplife/skip/controllers/SolicitudController.java
+++ Skip_Backend/src/main/java/com/simplife/skip/controllers/SolicitudController.java
@@ -1,14 +1,14 @@
 package com.simplife.skip.controllers;
 
 
+import com.simplife.skip.models.Solicitud;
 import com.simplife.skip.models.Usuario;
+import com.simplife.skip.payload.requests.SolicitudRequest;
 import com.simplife.skip.services.SolicitudService;
 import com.simplife.skip.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
