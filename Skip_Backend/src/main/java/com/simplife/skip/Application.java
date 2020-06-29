baa10a0ba970e081e5a319ca272c2339e33ea80b
Pepino <cardiacchino@gmail.com>
2020-06-28 23:09:42 -0500

    cambios ahora si corre

--- Skip_Backend/src/main/java/com/simplife/skip/Application.java
+++ Skip_Backend/src/main/java/com/simplife/skip/Application.java
@@ -11,7 +11,7 @@ import org.springframework.jmx.support.RegistrationPolicy;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class );
+        SpringApplication.run(Application.class);
     }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
