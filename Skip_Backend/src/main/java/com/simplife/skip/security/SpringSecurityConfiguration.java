2900cf602f5d990fd9b9705f1ae602285ba211f2
Sebastian <u201711033@upc.edu.pe>
2020-09-12 15:24:25 -0500

    commit de swagger corriendo y bailando huayno

--- Skip_Backend/src/main/java/com/simplife/skip/security/SpringSecurityConfiguration.java
+++ Skip_Backend/src/main/java/com/simplife/skip/security/SpringSecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -54,16 +55,21 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception{
 
         http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/servicios/lista").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().disable();
-
         http.cors();
         http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
